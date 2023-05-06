f9ffcfab99e330b302420dd9ff25ce01d7636801
christian.sonic <yoh.chrisitan@gmail.com>
2023-05-05 21:38:03 -0400

    Initial Commit

---
+++ Wallet complex UI/Wallet_complex_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wallet_complex_UIApp.swift
+//  Wallet complex UI
+//
+//  Created by Sonic on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Wallet_complex_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
